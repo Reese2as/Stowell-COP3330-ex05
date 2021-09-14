9089a0beefebf09d8bdb74d7ce42fb5f89d9d2ee
Reese2as <89660247+Reese2as@users.noreply.github.com>
2021-09-13 22:43:53 -0400

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,14 +9,17 @@ public class App {
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
         int num1, num2, ans1, ans2, ans3, ans4;
+        
         System.out.print("What is the first number? ");
         num1 = user_input.nextInt();
         System.out.print("What is the second number? ");
         num2 = user_input.nextInt();
+        
         ans1 = (num1 + num2);
         ans2 = (num1 - num2);
         ans3 = (num1 * num2);
         ans4 = (num1 / num2);
+        
         System.out.println(String.format("%d + %d = %d", num1, num2, ans1));
         System.out.println(String.format("%d - %d = %d", num1, num2, ans2));
         System.out.println(String.format("%d * %d = %d", num1, num2, ans3));
